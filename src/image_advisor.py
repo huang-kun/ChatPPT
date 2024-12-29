a37b4b28783f1cb8ffbfa943b6ff7a6f92bf122a
huang-kun <jack-huang-developer@foxmail.com>
2024-12-29 17:33:50 +0800

    fix: replace sd model with quantized model to save vram.

--- src/image_advisor.py
+++ src/image_advisor.py
@@ -13,12 +13,33 @@ from langchain_core.prompts import ChatPromptTemplate
 from logger import LOG  # 导入日志工具
 from minicpm_v_model import chat_with_image
 
-import torch
-from diffusers import StableDiffusion3Pipeline
-
 # 加载SD模型
-pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-medium", torch_dtype=torch.bfloat16)
-pipe = pipe.to("cuda")
+from diffusers import BitsAndBytesConfig, SD3Transformer2DModel
+from diffusers import StableDiffusion3Pipeline
+import torch
+
+# 为了保证顺利加载，这里采用了量化版本，减少显存消耗。
+
+model_id = "stabilityai/stable-diffusion-3.5-medium"
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+model_nf4 = SD3Transformer2DModel.from_pretrained(
+    model_id,
+    subfolder="transformer",
+    quantization_config=nf4_config,
+    torch_dtype=torch.bfloat16
+)
+pipeline = StableDiffusion3Pipeline.from_pretrained(
+    model_id, 
+    transformer=model_nf4,
+    torch_dtype=torch.bfloat16
+)
+pipeline.enable_model_cpu_offload()
+
 
 class ImageAdvisor(ABC):
     """
@@ -106,10 +127,11 @@ class ImageAdvisor(ABC):
             if score < 60:
                 LOG.debug(f"[Advisor 图片生成]: 正在生成中...")
 
-                image = pipe(
-                    query,
+                image = pipeline(
+                    prompt=query,
                     num_inference_steps=40,
                     guidance_scale=4.5,
+                    max_sequence_length=512,
                 ).images[0]
 
                 self.save_image(image, save_path)
